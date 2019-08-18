3f84602f5d410831bf755c4d5228ae2a4fec090f
zhangl <304355541@qq.com>
2019-08-18 22:21:28 +0800

    .

---
+++ ConsoleApp/AddTwoNumbersSolution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    public class AddTwoNumbersSolution
+    {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2)
+        {
+            int val = 0;
+            var pre = new ListNode(0);
+            var temp = pre;
+            while (l1 != null || l2 != null || val != 0)
+            {
+                val = val + (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+                temp.next = new ListNode(val % 10);
+                temp = temp.next;
+                val = val / 10;
+                l1 = l1 == null ? null : l1.next;
+                l2 = l2 == null ? null : l2.next;
+            }
+            return pre.next;
+        }
+
+        public static ListNode GenerateList(int[] vals)
+        {
+            ListNode cur = null;
+            ListNode temp = null;
+            foreach (var val in vals)
+            {
+                if (cur == null)
+                {
+                    cur = new ListNode(val);
+                    temp = cur;
+                }
+                else
+                {
+                    var next = new ListNode(val);
+                    temp.next = next;
+                    temp = next;
+                }
+            }
+            return cur;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+
+        public ListNode next;
+
+        public ListNode(int x) { val = x; }
+    }
+
+    
+}
---
+++ ConsoleApp/TwoSumSolution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ConsoleApp
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var rs = new List<int>();
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        if (!rs.Contains(i))
+                        {
+                            rs.Add(i);
+                        }
+                        if (!rs.Contains(j))
+                        {
+                            rs.Add(j);
+                        }
+                    }
+                }
+            }
+            return rs.ToArray();
+        }
+
+        public int[] TwoSumV2(int[] nums, int target)
+        {
+            var rs = new List<int>();
+            var list = nums.ToList();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var v = target - list[i];
+                if (list.Contains(v))
+                {
+                    var idx = Array.IndexOf(nums, v);
+                    if (idx != i)
+                    {
+                        if (!rs.Contains(idx))
+                        {
+                            rs.Add(idx);
+                        }
+                        if (!rs.Contains(i))
+                        {
+                            rs.Add(i);
+                        }                        
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            return rs.ToArray();
+        }
+    }
+}
