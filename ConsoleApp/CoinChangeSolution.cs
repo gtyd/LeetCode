22dde0ee4a7587854b24117f3987049e2847f26c
zhanglin <zhanglin@fulu.com>
2020-03-14 18:56:56 +0800

    .

---
+++ ConsoleApp/CoinChangeSolution.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class CoinChangeSolution
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="coins"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static int CoinChange(int[] coins, int amount)
+        {
+            int max = amount + 1;
+            int[] dp = new int[amount + 1];
+            Array.Fill(dp, max);
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++)
+            {
+                for (int j = 0; j < coins.Length; j++)
+                {
+                    if (coins[j] <= i) //i为总面值，枚举最后一枚硬币面额，当小于等于面额时才有效，即：coins[j]
+                    {
+                        dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+    }
+}
---
+++ NUnitTest/CoinChangeSolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Tests
+{
+    [TestFixture()]
+    public class CoinChangeSolutionTests
+    {
+        [Test()]
+        public void CoinChangeTest()
+        {
+            var coins = new[] {1, 3, 5};
+            var amount = 17;
+            var rs = CoinChangeSolution.CoinChange(coins,amount);
+            Assert.AreEqual(5, rs);
+        }
+    }
+}
